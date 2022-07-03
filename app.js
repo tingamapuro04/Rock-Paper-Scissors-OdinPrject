c3b493de951c4881d6892c273b5fd06bca95b764
tingamapuro04 <alponcemobutu100@gmail.com>
2022-07-03 22:33:10 +0300

    failure

--- app.js
+++ app.js
@@ -1,1 +1,57 @@
-const choice = document.querySelector('#choice').ariaValueMax.trim()
+let choice = document.querySelector('#choice').value.trim().toLocaleLowerCase();
+const form = document.querySelector('#submit');
+const rom = document.querySelector('.form')
+
+const computerPlay = (jatugo) => {
+  const compPlay =['Rock', 'Paper', 'Scissors'];
+  let guess = Math.floor(Math.random()*3);
+  const computer = compPlay[guess].toLocaleLowerCase();
+  let wins =0
+  if (jatugo !== '') {
+    if (jatugo === computer) {
+      wins ++
+      console.log(`Yeeeey! You won💯💯🎉✨ ${wins}`)
+    } else {
+      console.log("OOOOHHH 🥱😫 machine beats man again (❁´◡`❁)")
+    }
+  } else {
+    console.log("Please enter a value")
+  }
+  choice = ''
+};
+
+
+window.onload = computerPlay(choice)
+
+// rom.addEventListener('submit', (e) => {
+//   e.preventDefault()
+//   computerPlay(choice)
+// })
+
+// const computer = computerPlay();
+
+// let wins = 0;
+// const game = (User, Comp) => {
+//   if (User !== '') {
+//     if (User === Comp) {
+//     wins ++
+//     console.log(`Yeeeey! You won💯💯🎉✨ ${wins}`)
+//     } else {
+//       console.log("OOOOHHH 🥱😫 machine beats man again (❁´◡`❁)")
+//     }
+//   } else {
+//     console.log('Please enter a value')
+//   }
+  
+// }
+
+
+
+// form.addEventListener('click', (e) => {
+//   game(choice, computer)
+//   e.preventDefault()
+// })
+
+
+
+// game(choice, computer)
